8ce0edc3349032bfb6e8b602bd308b502895e7da
rasmvah <rasmvah@greeny.cs.tlu.ee>
2019-04-22 10:26:39 +0300

    Tehtud css-ile ümber ning mõned parandused

--- index.js
+++ index.js
@@ -1,47 +1,146 @@
-/*jshint esversion: 6*/
-
-class ToDo{
-    constructor(title, description, date){
-        this.title = title;
-        this.description = description;
-        this.date = date;
-        this.done = false;
-    }
+/*jshint esversion:6*/
+class Entry{
+  constructor(title, description, date){
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.done = false;
+  }
 }
+
+window.onload = function(){
+  render();
+  console.log("Function render has been initiated?");
+};
 
 let todos = [];
 
 $('#addButton').on('click', ()=>addEntry());
 $('#saveButton').on('click', ()=>saveToFile());
-$('#loadButton').on('click', ()=>render());
 
 function render(){
   $('#todos').html("");
   $.get('database.txt', function(data){
     let content = JSON.parse(data).content;
 
-    content.forEach(function(todo, todoIndex){
-      console.log(todoIndex);
-      $('#todos').append('<ul><li>' + todo.title + '</li><li>' + todo.description + '</li><li>' + todo.date + '</li></ul>');
+    content.forEach(function(entry, entryIndex){
+      console.log(entryIndex);
+      $('#todos').append('<ul><li>' + entry.title + '</li><li>' + entry.description + '</li><li>' + entry.date + '</li></ul>');
     });
   });
 }
 
 function addEntry(){
-    const titleValue = $('#title').val();
-    const dateValue = $('#date').val();
-    const descriptionValue = $('#description').val();
+  const titleValue = $('#title').val();
+  const dateValue = $('#date').val();
+  const descriptionValue = $('#description').val();
 
-    todos.push(new ToDo(titleValue, descriptionValue, dateValue));
-    console.log(todos);
+  todos.push(new Entry(titleValue, descriptionValue, dateValue));
+  console.log(todos);
 }
 
 function saveToFile(){
-    $.post('server.php', {save: todos}).done(function(){
-        alert("done");
-    }).fail(function(){
-        console.log("fail");
-    }).always(function(){
-        console.log("always");
-    });
+  $.post('server.php', {save: todos}).done(function(){
+    alert("Saved!");
+  }).fail(function(){
+    console.log("Failed to save!");
+  }).always(function(){
+    console.log("Reached the end of save to file function!");
+  });
 }
+
+class ToDo{
+  constructor(){
+    this.entries = JSON.parse(window.localStorage.getItem('entries')) || [];
+
+    document.querySelector('#addButton').addEventListener('click', () => this.addEntry());
+
+    this.render();
+  }
+
+  render(){
+    if(document.querySelector('.todo-list')){
+      document.body.removeChild(document.querySelector('.todo-list'));
+    }
+
+    const ul = document.createElement('ul');
+    ul.className = 'todo-list';
+
+    this.entries.forEach((entry, entryIndex)=>{
+      const li = document.createElement('li');
+      const removeTaskButton = document.createElement('div');
+      const removeIcon = document.createTextNode('\u00D7');
+
+      li.classList.add('entry');
+      removeTaskButton.className = "delete-task-button";
+
+      li.addEventListener('click', (event)=>{
+        event.target.classList.add('task-completed');
+
+        if(entry.done){
+          entry.done = false;
+        }else{
+          entry.done = true;
+        }
+
+        this.saveInLocalStorage();
+        this.render();
+      });
+
+      removeTaskButton.addEventListener('click', ()=>{
+        ul.removeChild(li);
+        this.entries = this.entries.slice(0, entryIndex).concat(this.entries.slice(entryIndex + 1, this.entries.length));
+        this.saveInLocalStorage();
+      });
+
+      if(entry.done){
+        li.style.backgroundColor = "lightgreen";
+        li.style.textDecoration = "line-through";
+      }
+
+      let today = new Date();
+      let dd = today.getDate();
+      let mm = today.getMonth() + 1;
+      let yyyy = today.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+
+      today = yyyy + '-' + mm + '-' + dd;
+      if(entry.date == today){
+        li.style.border = "2px solid red";
+      }
+
+      removeTaskButton.appendChild(removeIcon);
+      li.innerHTML = `${entry.title} <br> ${entry.description} <br> ${entry.date}`;
+      li.appendChild(removeTaskButton);
+      ul.appendChild(li);
+
+    });
+
+    document.body.appendChild(ul);
+  }
+
+  addEntry(){
+    const titleValue = document.querySelector('#title').value;
+    const descriptionValue = document.querySelector('#description').value;
+    const dateValue = document.querySelector('#date').value;
+
+    this.entries.push(new Entry(titleValue, descriptionValue, dateValue));
+
+    this.saveInLocalStorage();
+
+    this.render();
+  }
+
+  saveInLocalStorage(){
+    window.localStorage.setItem('entries', JSON.stringify(this.entries));
+  }
+}
+
+const todo = new ToDo();
--- server.php
+++ server.php
@@ -3,12 +3,12 @@
     saveToFile($_POST["save"]);
   }
   function saveToFile($stringToSave){
-    $object = new StdClass();
+    $object = new Stdclass();
     $object->last_modified = time();
     $object->content = $stringToSave;
     $jsonString = json_encode($object);
     if(file_put_contents("database.txt", $jsonString)){
-      echo "Success";
+      echo "Success!";
     }
   }
 ?>
